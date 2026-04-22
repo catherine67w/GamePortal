90c68b8eb2e1e924e974c117bcef57ea25da22f8
Catherine Wang <catherine_wang@horacemann.org>
2026-04-21 22:15:09 -0400

    added Buzzfeed quiz and blackjack

---
+++ src/src/Answer.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Answer {
+    String label;
+    Category cat;
+    
+    // Constructor
+    Answer(String label, Category c) {
+        this.label = label; 
+        this.cat = c;
+    }
+}
---
+++ src/src/Category.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Category {
+
+    String label;
+    String description; // after user is done and gets this category, will output description.
+    int points = 0;
+
+    Category(String c, String description) {
+        this.label = c;
+        this.description = description;
+    }
+
+}
